1c04bdf55f34d6c387b2c4f093203dd32e9c7960
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-04 15:06:03 -0500

    feat: Update login logic to use connect.sid cookie for authentication

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -13,19 +13,16 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
-        const token = Cookies.get('TrujiStudios'); // esta linea es la que se cambia por la cookie que se va a usar
+        const token = Cookies.get('connect.sid'); // esta linea es la que se cambia por la cookie que se va a usar
         console.log('Initial Token Check:', !!token);
         return !!token;
     });
 
     const login = async () => {
-        // Simulate login logic
         // Cookies.set('token', document.cookie);
-        console.log('Token <>:', Cookies.get('TrujiStudios'));
-
-
-        if (Cookies.get('TrujiStudios')) {
-            console.log('Token Check:', !!Cookies.get('TrujiStudios')); // Log for debugging
+        console.log('Token <> <>:', Cookies.get('connect.sid'));
+        if (Cookies.get('connect.sid')) {
+            console.log('Token Check:', !!Cookies.get('connect.sid')); // Log for debugging
             setIsAuthenticated(true);
         }
 
@@ -34,6 +31,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     const logout = () => {
         // Cookies.remove('isAuthenticated');
         Cookies.remove('TrujiStudios');
+        Cookies.remove('connect.sid');
         setIsAuthenticated(false);
     };
 
