b7bca87c3a88be3d1d9e125063f535b779c340df
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-05 14:43:03 -0500

    feat: Actualizar llamadas a la API en ProductHook.tsx

--- src/components/ManagementSystem/products/Products.tsx
+++ src/components/ManagementSystem/products/Products.tsx
@@ -1,24 +1,68 @@
 import {
-    Container, TextField, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box, IconButton, Dialog, DialogTitle, DialogContent, DialogActions, TablePagination, Select, MenuItem,
+    Container,
+    TextField,
+    Button,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Box,
+    IconButton,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    TablePagination,
+    Select,
+    MenuItem,
     FormControlLabel,
     Switch
-} from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { ProductHook } from '../../../hook/ProductHook';
-
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { ProductHook } from "../../../hook/ProductHook";
 
 const Products: React.FC = () => {
-    const { productos, categorias, handleOpenModal, handleCloseModal, handleSave, handleDelete, handleChangePage, handleChangeRowsPerPage, handleInputChange, handleStatusChange, selectedProducto, openModal, editMode, page, rowsPerPage } = ProductHook();
-
+    const {
+        productos,
+        categorias,
+        handleOpenModal,
+        handleCloseModal,
+        handleSave,
+        handleDelete,
+        handleChangePage,
+        handleChangeRowsPerPage,
+        handleInputChange,
+        handleStatusChange,
+        selectedProducto,
+        openModal,
+        editMode,
+        page,
+        rowsPerPage
+    } = ProductHook();
 
     return (
         <Container>
             <h2>Productos</h2>
 
-            <Box mt={10} display="flex" justifyContent="space-between" alignItems="center" mb={2}>
+            <Box
+                mt={10}
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                mb={2}
+            >
                 <TextField label="Buscar:" variant="outlined" size="small" />
-                <Button variant="contained" color="secondary" onClick={() => handleOpenModal()}>Nuevo</Button>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => handleOpenModal()}
+                >
+                    Nuevo
+                </Button>
             </Box>
             <Paper>
                 <TableContainer>
@@ -34,33 +78,41 @@ const Products: React.FC = () => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {productos.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((producto) => (
-                                <TableRow key={producto._id}>
-                                    <TableCell>{producto.name}</TableCell>
-                                    <TableCell>{producto.description}</TableCell>
-                                    <TableCell>{producto.price}</TableCell>
-                                    <TableCell>{producto.category[1]}</TableCell>
-                                    {/* <TableCell>{categorias.find(categoria => categoria._id === producto.category)?.name || 'N/A'}</TableCell> */}
-                                    <TableCell>
-                                        <Button
-                                            variant="contained"
-                                            color={producto.status ? 'success' : 'error'}
-                                            size="small"
-                                        >
-                                            {producto.status ? 'Activo' : 'Inactivo'}
-                                        </Button>
-                                        {/* <Button variant="contained" color="success" size="small">{producto.status ? 'Activo' : 'Inactivo'}</Button> */}
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <IconButton color="primary" onClick={() => handleOpenModal(producto)}>
-                                            <EditIcon />
-                                        </IconButton>
-                                        <IconButton color="error" onClick={() => handleDelete(producto._id)}>
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
+                            {productos
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((producto) => (
+                                    <TableRow key={producto._id}>
+                                        <TableCell>{producto.name}</TableCell>
+                                        <TableCell>{producto.description}</TableCell>
+                                        <TableCell>{producto.price}</TableCell>
+                                        <TableCell>{producto.category[1]}</TableCell>
+                                        {/* <TableCell>{categorias.find(categoria => categoria._id === producto.category)?.name || 'N/A'}</TableCell> */}
+                                        <TableCell>
+                                            <Button
+                                                variant="contained"
+                                                color={producto.status ? "success" : "error"}
+                                                size="small"
+                                            >
+                                                {producto.status ? "Activo" : "Inactivo"}
+                                            </Button>
+                                            {/* <Button variant="contained" color="success" size="small">{producto.status ? 'Activo' : 'Inactivo'}</Button> */}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <IconButton
+                                                color="primary"
+                                                onClick={() => handleOpenModal(producto)}
+                                            >
+                                                <EditIcon />
+                                            </IconButton>
+                                            <IconButton
+                                                color="error"
+                                                onClick={() => handleDelete(producto._id)}
+                                            >
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
@@ -76,7 +128,9 @@ const Products: React.FC = () => {
             </Paper>
 
             <Dialog open={openModal} onClose={handleCloseModal}>
-                <DialogTitle>{editMode ? 'Editar Producto' : 'Nuevo Producto'}</DialogTitle>
+                <DialogTitle>
+                    {editMode ? "Editar Producto" : "Nuevo Producto"}
+                </DialogTitle>
                 <DialogContent>
                     <TextField
                         autoFocus
@@ -84,7 +138,7 @@ const Products: React.FC = () => {
                         label="Nombre"
                         name="name"
                         fullWidth
-                        defaultValue={selectedProducto?.name || ''}
+                        defaultValue={selectedProducto?.name || ""}
                         onChange={handleInputChange}
                     />
                     <TextField
@@ -92,7 +146,7 @@ const Products: React.FC = () => {
                         label="Descripción"
                         name="description"
                         fullWidth
-                        defaultValue={selectedProducto?.description || ''}
+                        defaultValue={selectedProducto?.description || ""}
                         onChange={handleInputChange}
                     />
                     <TextField
@@ -101,7 +155,7 @@ const Products: React.FC = () => {
                         name="price"
                         type="number"
                         fullWidth
-                        defaultValue={selectedProducto?.price || ''}
+                        defaultValue={selectedProducto?.price || ""}
                         onChange={handleInputChange}
                     />
                     <Select
@@ -111,8 +165,10 @@ const Products: React.FC = () => {
                         value={selectedProducto?.category[0]}
                         onChange={handleInputChange}
                     >
-                        {categorias.map(categoria => (
-                            <MenuItem key={categoria.id} value={categoria.id}>{categoria.name}</MenuItem>
+                        {categorias.map((categoria) => (
+                            <MenuItem key={categoria.id} value={categoria.id}>
+                                {categoria.name}
+                            </MenuItem>
                         ))}
                     </Select>
                     <TextField
@@ -120,7 +176,7 @@ const Products: React.FC = () => {
                         label="URL de la Imagen"
                         name="image"
                         fullWidth
-                        defaultValue={selectedProducto?.image || ''}
+                        defaultValue={selectedProducto?.image || ""}
                         onChange={handleInputChange}
                     />
                     <TextField
@@ -128,7 +184,7 @@ const Products: React.FC = () => {
                         label="Código"
                         name="code"
                         fullWidth
-                        defaultValue={selectedProducto?.code || ''}
+                        defaultValue={selectedProducto?.code || ""}
                         onChange={handleInputChange}
                     />
 
@@ -142,11 +198,14 @@ const Products: React.FC = () => {
                         }
                         label="Estado"
                     />
-
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleCloseModal} color="primary">Cancelar</Button>
-                    <Button onClick={handleSave} color="primary">{editMode ? 'Guardar Cambios' : 'Crear'}</Button>
+                    <Button onClick={handleCloseModal} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={handleSave} color="primary">
+                        {editMode ? "Guardar Cambios" : "Crear"}
+                    </Button>
                 </DialogActions>
             </Dialog>
         </Container>
