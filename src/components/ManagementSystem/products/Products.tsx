e0fccbda47b4e0939a100f9b8826a110de25bac9
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-05 13:54:41 -0500

    feat: Update import path for Products component in ManagementSystem

--- src/components/ManagementSystem/products/Products.tsx
+++ src/components/ManagementSystem/products/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import {
     Container, TextField, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box, IconButton, Dialog, DialogTitle, DialogContent, DialogActions, TablePagination, Select, MenuItem,
     FormControlLabel,
@@ -6,143 +6,13 @@ import {
 } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import axios from 'axios';
+import { ProductHook } from '../../../hook/ProductHook';
+// import { ProductHook } from '../../../hook/ProductHook';
 
-interface Categoria {
-    id: string;
-    name: string;
-}
-
-interface Producto {
-    _id: string;
-    name: string;
-    description: string;
-    price: number;
-    category: string;
-    status: boolean;
-    image: string;
-    code: string;
-}
-
-const emptyProducto: Producto = {
-    _id: '',
-    name: '',
-    description: '',
-    price: 0,
-    category: '',
-    status: false,
-    image: '',
-    code: '',
-};
-
-const initialProductos: Producto[] = [];
-const initialCategorias: Categoria[] = [];
 
 const Products: React.FC = () => {
-    const [productos, setProductos] = useState<Producto[]>(initialProductos);
-    const [categorias, setCategorias] = useState<Categoria[]>(initialCategorias);
-    const [openModal, setOpenModal] = useState<boolean>(false);
-    const [editMode, setEditMode] = useState<boolean>(false);
-    const [selectedProducto, setSelectedProducto] = useState<Producto | null>(null);
-    const [page, setPage] = useState<number>(0);
-    const [rowsPerPage, setRowsPerPage] = useState<number>(5);
+    const { productos, categorias, handleOpenModal, handleCloseModal, handleSave, handleDelete, handleChangePage, handleChangeRowsPerPage, handleInputChange, handleStatusChange, selectedProducto, openModal, editMode, page, rowsPerPage } = ProductHook();
 
-    useEffect(() => {
-        fetchProductos();
-        fetchCategorias();
-    }, []);
-
-    const fetchProductos = async (): Promise<void> => {
-        try {
-            const response = await axios.get('http://localhost:5000/api/v1/product/get', { withCredentials: true });
-            setProductos(response.data);
-        } catch (error) {
-            console.error('Error fetching productos:', error);
-        }
-    };
-
-    const fetchCategorias = async (): Promise<void> => {
-        try {
-            const response = await axios.get('http://localhost:5000/api/v1/product/category/get', { withCredentials: true });
-            setCategorias(response.data);
-        } catch (error) {
-            console.error('Error fetching categorias:', error);
-        }
-    };
-
-
-    const handleOpenModal = (producto: Producto | null = null): void => {
-        setSelectedProducto(producto || emptyProducto);
-        setEditMode(producto !== null);
-        setOpenModal(true);
-    };
-
-    const handleCloseModal = (): void => {
-        setOpenModal(false);
-        setSelectedProducto(null);
-    };
-
-    const handleSave = async (): Promise<void> => {
-        if (editMode && selectedProducto) {
-            try {
-
-                if (selectedProducto) {
-                    const productoToSend = {
-                        ...selectedProducto,
-                        category: selectedProducto.category[0]
-                    };
-                    await axios.put(`http://localhost:5000/api/v1/product/update/${selectedProducto._id}`, productoToSend, { withCredentials: true });
-                }
-
-
-            } catch (error) {
-                console.error('Error updating producto:', error);
-            }
-        } else {
-            try {
-                console.log("NUEVO PRODUCTO", selectedProducto);
-                await axios.post('http://localhost:5000/api/v1/product/create', selectedProducto, { withCredentials: true });
-            } catch (error) {
-                console.error('Error creating producto:', error);
-            }
-        }
-        handleCloseModal();
-        fetchProductos();
-    };
-
-    const handleDelete = async (id: string): Promise<void> => {
-        try {
-            await axios.delete(`http://localhost:5000/api/v1/product/delete/${id}`, { withCredentials: true });
-            fetchProductos();
-        } catch (error) {
-            console.error('Error deleting producto:', error);
-        }
-    };
-
-    const handleChangePage = (newPage: number): void => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<{ name?: string; value: unknown }>): void => {
-        const { name, value } = event.target;
-        // setSelectedProducto(prevProducto => prevProducto ? { ...prevProducto, [name as string]: value } : null);
-        setSelectedProducto(prevProducto => prevProducto ? { ...prevProducto, [name as string]: value } : null);
-        // setSelectedProducto(prevProducto => prevProducto ? { ...prevProducto, [name as string]: { ...emptyProducto, [name]: value } } : null);
-        // setSelectedProducto(prevProducto => prevProducto ? { ...prevProducto, [name as string]: value } : null);
-    };
-
-    // console.log('productos:', producto.category);
-
-    const handleStatusChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setSelectedProducto(prevProducto => prevProducto ? { ...prevProducto, status: event.target.checked } : { status: event.target.checked } as Producto);
-        // setSelectedCategoria(prevCategoria => prevCategoria ? { ...prevCategoria, status: event.target.checked } : { status: event.target.checked } as Category);
-
-    }
 
     return (
         <Container>
