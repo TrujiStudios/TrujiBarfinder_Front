8f87554a9b7f55e7ffc6dbc6d683aa8b1e894970
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-05 09:56:15 -0500

    feat: Update import path for Products component in ManagementSystem

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ import SignIn from "../components/SignIn/SignIn";
 // import { Dashboard } from "../components/ManagementSystem/Dashboard";
 // import BarManagement from "../components/ManagementSystem/appBarManagement/BarManagement";
 import { Verder } from "../components/ManagementSystem/Verder";
-import { Products } from "../components/ManagementSystem/Products";
+import Products from "../components/ManagementSystem/products/Products";
 import ProtectedRoute from "../ProtectedRoute";
 import { Dashboard } from "../components/ManagementSystem/Dashboard";
 import Category from "../components/ManagementSystem/category/Category";
