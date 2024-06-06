b5ea027055b3d3016dcd307d4016121a07450b3a
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-06 15:25:12 -0500

    feat: Actualizar estilos de la tabla en el componente Mesas

---
+++ src/themes/tableTheme.tsx
@@ -0,0 +1,10 @@
+export const styles = {
+  tableCellHeader: {
+    fontWeight: "bold",
+    fontSize: "1.2em"
+  },
+  tableCellTable: {
+    fontSize: "1em" // 1em
+    // fontWeight: "bold"
+  }
+};
