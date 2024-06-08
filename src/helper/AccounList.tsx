c804b38e403be9c74a99ad1e8c96b75356e1beb6
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-08 13:40:06 -0500

    feat: Add new icons to account list in AccounList.tsx

--- src/helper/AccounList.tsx
+++ src/helper/AccounList.tsx
@@ -1,12 +1,34 @@
-import TabletIcon from '@mui/icons-material/Tablet';
-import ContactPageRoundedIcon from '@mui/icons-material/ContactPageRounded';
+import TabletIcon from "@mui/icons-material/Tablet";
+import ContactPageRoundedIcon from "@mui/icons-material/ContactPageRounded";
+import RedeemTwoToneIcon from "@mui/icons-material/RedeemTwoTone";
+import CategoryTwoToneIcon from "@mui/icons-material/CategoryTwoTone";
 
 export const accountList = [
-    { label: 'Mesas', icon: <TabletIcon sx={{ fontSize: 20 }} />, route: '/dashboard/tables' },
-    { label: 'My account', icon: <ContactPageRoundedIcon sx={{ fontSize: 20 }} />, route: '/dashboard/category' },
+  {
+    label: "Mesas",
+    icon: <TabletIcon sx={{ fontSize: 20 }} />,
+    route: "/dashboard/tables"
+  },
+  {
+    label: "My account",
+    icon: <ContactPageRoundedIcon sx={{ fontSize: 20 }} />,
+    route: "/dashboard/category"
+  }
 ];
 
 export const accountListProduct = [
-    { label: 'Categoria', icon: <TabletIcon sx={{ fontSize: 20, marginLeft: 3, textDecoration: "none" }} />, route: '/dashboard/category' },
-    { label: 'Productos', icon: <ContactPageRoundedIcon sx={{ fontSize: 20, marginLeft: 3 }} />, route: '/dashboard/product' },
+  {
+    label: "Categoria",
+    icon: (
+      <CategoryTwoToneIcon
+        sx={{ fontSize: 20, marginLeft: 3, textDecoration: "none" }}
+      />
+    ),
+    route: "/dashboard/category"
+  },
+  {
+    label: "Productos",
+    icon: <RedeemTwoToneIcon sx={{ fontSize: 20, marginLeft: 3 }} />,
+    route: "/dashboard/product"
+  }
 ];
