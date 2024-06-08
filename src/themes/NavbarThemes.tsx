d2850877d1c399ed357e7680e1f22edbb5c425c2
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-08 13:34:04 -0500

    chore: Update NavbarThemes.tsx to improve toolbar styling and layout

--- src/themes/NavbarThemes.tsx
+++ src/themes/NavbarThemes.tsx
@@ -1,8 +1,28 @@
+import { styled } from "@mui/material/styles";
+import MuiToolbar from "@mui/material/Toolbar";
 
-
+// eslint-disable-next-line react-refresh/only-export-components
 export const styles = {
-    linkNavbar: {
-        textDecoration: 'none',
-        color: 'inherit',
-    },
-}
+  linkNavbar: {
+    textDecoration: "none",
+    color: "inherit"
+  }
+};
+
+export const Toolbar = styled(MuiToolbar)({
+  maxWidth: 1538,
+  width: "100%",
+  padding: "16px 16px 0 16px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "start",
+  justifyContent: "center",
+  gap: "12px",
+  flexShrink: 0,
+  backdropFilter: "blur(24px)",
+  "& .MuiTabs-flexContainer": {
+    gap: "8px",
+    p: "8px",
+    pb: 0
+  }
+});
