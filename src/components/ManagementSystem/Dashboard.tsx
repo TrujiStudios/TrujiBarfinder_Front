3042b48a295c11875882ea99da45695d0c7fdbd2
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-08 17:50:17 -0500

    feat: Improve styling and layout of Dashboard component

--- src/components/ManagementSystem/Dashboard.tsx
+++ src/components/ManagementSystem/Dashboard.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export const Dashboard = () => {
-    return (
-        <div style={{
-            color: 'red'
-        }}>
-            <h1 >Dashboard</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ullam illo eveniet eius soluta, sit nisi in, reprehenderit, cupiditate pariatur consectetur! Temporibus ullam esse beatae ex delectus. Consequuntur, quo a.</p>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        color: "red",
+        marginTop: "10em"
+      }}
+    >
+      <h1>Dashboard</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ullam illo
+        eveniet eius soluta, sit nisi in, reprehenderit, cupiditate pariatur
+        consectetur! Temporibus ullam esse beatae ex delectus. Consequuntur, quo
+        a.
+      </p>
+    </div>
+  );
+};
