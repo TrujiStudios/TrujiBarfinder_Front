654dea1a66e16513d9d711d02793389973488784
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-07-22 23:21:30 -0500

    feat: Add Order interface for managing orders and related entities

---
+++ src/interface/orderInterface.tsx
@@ -0,0 +1,47 @@
+export interface Order {
+  id: string;
+  company: string;
+  userId: string;
+  tableId: string;
+  status: string;
+  total: number;
+  products: Product[];
+  table: Table[];
+  user: User[];
+}
+
+export interface Product {
+  _id: string;
+  name: string;
+  description: string;
+  price: string;
+  category: string;
+  status: boolean;
+  image: string;
+  code: string;
+  company: string;
+}
+
+export interface Table {
+  _id: string;
+  name: string;
+  company: string;
+  description: string;
+  status: boolean;
+  image: string;
+  occupied?: boolean;
+}
+
+export interface User {
+  _id: string;
+  name: string;
+  lastName: string;
+  documentType: string;
+  typePerson: string;
+  email: string;
+  password: string;
+  phone: string;
+  role: string;
+  company: string;
+  status: boolean;
+}
