a5a443abf2401c065a6ae7acf6ab17e560392c18
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-09-17 03:32:35 -0500

    feat: Actualizar componente Sell para mostrar disponibilidad de mesas y estado de la orden

--- src/components/ManagementSystem/vender/Sell.tsx
+++ src/components/ManagementSystem/vender/Sell.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, Grid, Typography, Modal, Button } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  Modal,
+  Button,
+  Switch,
+  TextField
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import { OrdertHook } from "../../../hook/OrderHook";
 import { Order, Table } from "../../../interface/orderInterface";
+
 const DEFAULT_IMAGE_URL =
   "https://restobar.loggro.com/assets/images/pirpos/pirpos_table_dnd.png";
 const DEFAULT_IMAGE_URL_VERDE =
@@ -46,6 +55,20 @@ const Sell: React.FC = () => {
       | Order
       | undefined;
   };
+  // const getTableOrder = (tableId: string): Order | undefined => {
+  //   return order?.find((order) => order.tableId === tableId) as
+  //     | Order
+  //     | undefined;
+  // };
+
+  const formatPrice = (price: number): string => {
+    return new Intl.NumberFormat("es-CO", {
+      style: "currency",
+      currency: "COP",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    }).format(price);
+  };
 
   return (
     <Container
@@ -80,7 +103,9 @@ const Sell: React.FC = () => {
                     variant="h6"
                     onClick={() => order && handleOrderClick(order)}
                   >
-                    {order ? `Ocupada - Total: $${order.total}` : "Disponible"}
+                    {order
+                      ? `Ocupada - Total: ${formatPrice(order.total)}`
+                      : "Disponible"}
                   </Typography>
                 </Grid>
               );
@@ -100,41 +125,159 @@ const Sell: React.FC = () => {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width: 400,
+            width: "60%",
+            maxWidth: 900,
             backgroundColor: "white",
-            padding: 20,
-            // boxShadow: 24,
-            borderRadius: 8
+            borderRadius: 8,
+            padding: "20px",
+            overflow: "auto",
+            height: "auto",
+            boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.2)"
           }}
         >
-          <Typography id="order-modal-title" variant="h6">
-            {tables
-              ? `Orden de ${
-                  tables.find((table) => table.id === selectedOrder?.tableId)
-                    ?.name
-                }`
-              : "No hay orden"}
-          </Typography>
-          <Typography id="order-modal-description" sx={{ mt: 2 }}>
+          <Box
+            style={{
+              backgroundColor: "#C62828", // Color rojo de la cabecera
+              color: "white",
+              padding: "10px 20px",
+              borderTopLeftRadius: "8px",
+              borderTopRightRadius: "8px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center"
+            }}
+          >
+            <Typography variant="h6">
+              {tables
+                ? `Orden de ${
+                    tables.find((table) => table.id === selectedOrder?.tableId)
+                      ?.name
+                  }`
+                : "No hay orden"}
+            </Typography>
+            <Switch color="default" />
+          </Box>
+
+          <Box padding="20px">
             {selectedOrder ? (
               <>
-                {selectedOrder.products.map((product, index) => (
-                  <div key={index}>
-                    {product.name} - ${product.price}
-                  </div>
-                ))}
-                <div>
-                  <strong>{selectedOrder.quantity}</strong>
-                </div>
-                <div>
-                  <strong>Total: ${selectedOrder.total}</strong>
-                </div>
+                <Grid
+                  container
+                  spacing={2}
+                  style={{ borderBottom: "1px solid #e0e0e0" }}
+                >
+                  {selectedOrder.products.map((product, index) => (
+                    <Grid
+                      container
+                      spacing={2}
+                      key={index}
+                      alignItems="center"
+                      style={{ marginBottom: "10px" }}
+                    >
+                      <Grid item xs={2}>
+                        <Switch color="default" defaultChecked />
+                      </Grid>
+                      {/* <Grid item xs={2}>
+                        <Typography variant="body1">
+                          {new Date(product.createdAt).toLocaleString()}
+                        </Typography>
+                      </Grid> */}
+                      <Grid item xs={1}>
+                        <Typography variant="body1">
+                          {product.quantity}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Typography variant="body1">{product.name}</Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography variant="body1">
+                          {formatPrice(Number(product.price))}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={1}>
+                        {/* <Typography variant="body1">
+                          ${product.promo || 0}
+                        </Typography> */}
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography variant="body1">
+                          {formatPrice(
+                            product.quantity * Number(product.price)
+                          )}
+                        </Typography>
+                      </Grid>
+                      {/* <Grid item xs={1}>
+                        <Typography variant="body1">
+                          {product.seller}
+                        </Typography>
+                      </Grid> */}
+                      {/* <Grid item xs={2}>
+                        <Button variant="contained" color="error">
+                          Cancelar
+                        </Button>
+                      </Grid> */}
+                    </Grid>
+                  ))}
+                </Grid>
+
+                {/* Subtotal y descuentos */}
+                <Box mt={2}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={9}>
+                      <Typography variant="body1">Subtotal</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Typography variant="body1">
+                        {/* ${selectedOrder.total} */}
+                        {formatPrice(selectedOrder.total)}
+                      </Typography>
+                    </Grid>
+
+                    <Grid item xs={9}>
+                      <TextField
+                        label="Descuento (%)"
+                        size="small"
+                        variant="outlined"
+                        style={{ width: "100%" }}
+                      />
+                    </Grid>
+                    <Grid item xs={3}>
+                      <TextField
+                        label="Descuento ($)"
+                        size="small"
+                        variant="outlined"
+                        style={{ width: "100%" }}
+                      />
+                    </Grid>
+                  </Grid>
+                </Box>
+
+                {/* Total */}
+                <Box mt={2} display="flex" justifyContent="flex-end">
+                  <Typography variant="h6">
+                    Total: {formatPrice(selectedOrder.total)}
+                  </Typography>
+                </Box>
+
+                {/* Opciones adicionales */}
+                <Box mt={2} display="flex" justifyContent="space-between">
+                  <Box display="flex" alignItems="center">
+                    <Switch />
+                    <Typography variant="body1">Mover a otra mesa</Typography>
+                  </Box>
+                  <Box display="flex" alignItems="center">
+                    <Switch />
+                    <Typography variant="body1">Es cortesía</Typography>
+                  </Box>
+                </Box>
               </>
             ) : (
-              "No hay productos en esta orden."
+              <Typography variant="body1">
+                No hay productos en esta orden.
+              </Typography>
             )}
-          </Typography>
-          <Button onClick={handleCloseModal}>Cerrar</Button>
+          </Box>
         </Box>
       </Modal>
     </Container>
